58a4bc7051577f0979b916452ffc2c50208d8d0e
azanbinzahid <azan.zahid@arbisoft.com>
2020-09-28 14:57:39 +0500

    fix width

--- src/components/SectionHeading/SectionHeading.js
+++ src/components/SectionHeading/SectionHeading.js
@@ -3,9 +3,11 @@ import "./SectionHeading.css";
 function SectionHeading(props) {
   const { variant = "primary", children, ...rest } = props;
   return (
-    <p className={`section-heading ${variant}`} {...rest}>
-      {children}
-    </p>
+    <div align="center">
+      <p className={`section-heading ${variant}`} {...rest}>
+        {children}
+      </p>
+    </div>
   );
 }
 
